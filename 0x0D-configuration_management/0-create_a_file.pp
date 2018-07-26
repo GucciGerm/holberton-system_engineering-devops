e7042dad1ce1270506ca78d52a26a220c3953c53
[GucciGerm] <germaine.balmania@gmail.com>
2018-07-26 15:10:05 -0700

    Adding newly created puppet file

---
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Using puppet, we will create a file within the tmp file
+file {'/tmp/holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
