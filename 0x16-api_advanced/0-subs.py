ee90088737c95bef03775ae3f2320095ef850dd8
[GucciGerm] <germaine.balmania@gmail.com>
2018-08-30 12:15:03 -0700

    Adjusted documentation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,12 +10,12 @@ import requests
 def number_of_subscribers(subreddit):
 
     """
-    number_subscribers - This function will querie the reddit API and
-    will return the number of subscribers (not active, total users)
+        number_subscribers - This function will querie the reddit API and
+        will return the number of subscribers (not active, total users)
 
-    subreddit: This is the Reddit API
+        subreddit: This is the Reddit API
 
-    Return: If invalid return 0, else return the number of subscribers
+        Return: If invalid return 0, else return the number of subscribers
 
     """
 
