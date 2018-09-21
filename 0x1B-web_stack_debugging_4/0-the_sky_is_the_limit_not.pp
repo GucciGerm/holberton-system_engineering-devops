a8d5235894e1004a66d6ff3249f999a7cda2b404
GucciGerm <germaine.balmania@gmail.com>
2018-09-21 03:43:09 +0000

    Adding debugging solution for 1035 failures

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# By applying this puppet script will increase the use of sys resources
+
+exec { 'increase sys resources to 15000':
+    path    => '/bin',
+    command => 'sed -i '\s/15/15000/g'\ /etc/default/nginx',
+}
+exec { 'restart your nginx':
+    path    => '/usr/bin',
+    command => 'sudo service nginx restart',
+}
